0fc7afcbf29f3c0357fd77ff7e5b2cc24a2a147d
lieautman <67701909+lieautman@users.noreply.github.com>
2021-08-16 17:10:38 +0000

    basic

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print(10)
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("i am redy as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('Arcadius'):
+    await message.channel.send('Hello!')
+
+
+
+client.run(os.environ['TOKEN'])
