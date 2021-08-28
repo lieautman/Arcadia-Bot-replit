4c3f83e770c4aa02d4be274e24b5696a689ad7e3
lieautman <67701909+lieautman@users.noreply.github.com>
2021-08-28 18:44:17 +0000

    nithing

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 import os
 from keep_alive import keep_alive
 import datetime
+from replit import db
+
 
 
 #intents to let on_member_join/on_member_remove work
@@ -11,7 +13,7 @@ intents = discord.Intents.default()
 intents.members = True
 
 #client instance initiated
-client = commands.Bot(command_prefix = 'Arcadius ', intents=intents)
+client = commands.Bot(command_prefix = {'Arcadius ','arcadius ','arc ','Arc ','ar ','Ar '}, intents=intents)
 
 #redy message
 @client.event
@@ -19,24 +21,31 @@ async def on_ready():
   print("i am redy as {0.user}".format(client))
 
 
+#get time for storage
+now=datetime.datetime.now()
+print("Current date and time is:")
+print(now.strftime("%y-%m-%d %H:%M:%S"))
+
+
 #on join
 @client.event
 async def on_member_join(member):
-  print(f'{member} has joined the server.')
+  now=datetime.datetime.now()
+  print(f'{member} has joined the server at ')
+  print(now.strftime("%y-%m-%d %H:%M:%S."))
+  db[f'{member}'] = now.strftime("%y-%m-%d %H:%M:%S.")
+  keys = db.keys()
+  print(keys)
 
 #on leave
 @client.event
 async def on_member_remove(member):
-  print(f'{member} has left the server.')
-
-#first try
-#@client.event
-#async def on_message(message):
-#  if message.author == client.user:
-#    return
-
-  #if message.content.startswith('Arcadius'):
-  #  await message.channel.send('Hello!')
+  now=datetime.datetime.now()
+  print(f'{member} has left the server at ')
+  print(now.strftime("%y-%m-%d %H:%M:%S."))
+  del db[f'{member}']
+  keys = db.keys()
+  print(keys)
 
 
 #clear text command
@@ -49,6 +58,12 @@ async def clear(ctx, amount=5):
 @client.command()
 async def kick(ctx, member : discord.Member,*, reason=None):
   await member.kick(reason=reason)
+
+#hi command
+@client.command()
+async def hello(ctx):
+  await ctx.channel.send('Hello!')
+
 
 
 #keep alive loop
